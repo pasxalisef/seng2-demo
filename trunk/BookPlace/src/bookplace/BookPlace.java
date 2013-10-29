445a2d5a7880570a18e73d95c0f9f1e728b83992
nikos.seng2 <nikos.seng2@21861ad4-47e6-38e2-be5c-937554d057b9>
2013-10-29 17:55:21 +0000

    Initial import of project

---
+++ trunk/BookPlace/src/bookplace/BookPlace.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookplace;
+
+/**
+ *
+ * @author npetalidis
+ */
+public class BookPlace {
+
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+       
+    }
+}
